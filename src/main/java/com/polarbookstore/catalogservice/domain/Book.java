fdfea1fac90de5d74bbc4a3a802bde8b8c8c3653
Haoyang Guo <hyg.haoyang@gmail.com>
2024-06-19 23:49:08 -0700

    feat(db): test flyway db migration

--- src/main/java/com/polarbookstore/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookstore/catalogservice/domain/Book.java
@@ -11,33 +11,20 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 
 public record Book(
-    @Id
-    Long id,
-
+    @Id Long id,
     @NotBlank(message = "The book ISBN must be defined.")
-    @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
-    String isbn,
-
-    @NotBlank(message = "The book title must be defined.")
-    String title,
-
-    @NotBlank(message = "The book author must be defined.")
-    String author,
-
+        @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
+        String isbn,
+    @NotBlank(message = "The book title must be defined.") String title,
+    @NotBlank(message = "The book author must be defined.") String author,
     @NotNull(message = "The book price must be defined.")
-    @Positive(message = "The book price must be greater than zero.")
-    Double price,
-
-    @CreatedDate
-    Instant createdDate,
-
-    @LastModifiedDate
-    Instant lastModifiedDate,
-
-    @Version
-    int version
-) {
-   public static Book of(String isbn, String title, String author, Double price) {
-      return new Book(null, isbn, title, author, price, null, null, 0);
-   }
+        @Positive(message = "The book price must be greater than zero.")
+        Double price,
+    String publisher,
+    @CreatedDate Instant createdDate,
+    @LastModifiedDate Instant lastModifiedDate,
+    @Version int version) {
+  public static Book of(String isbn, String title, String author, Double price, String publisher) {
+    return new Book(null, isbn, title, author, price, publisher, null, null, 0);
+  }
 }
